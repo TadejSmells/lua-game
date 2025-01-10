f1081971882644f7a96eb7bbe95abb50f5c9dc24
Tadej <tadej.kadivec1@gmail.com>
2025-01-10 11:21:32 +0100

    njasd

--- main.lua
+++ main.lua
@@ -10,68 +10,68 @@ require("starting-screen.settings")
 
 
 function love.load()
-  menu:load()
-  settings:load()
-  players:load()
-  tower:load()
-  enemy:load()
-  loadmap:load()
-  --shoot:load()
+    menu:load()
+    settings:load()
+    players:load()
+    tower:load()
+    enemy:load()
+    loadmap:load()
+    --shoot:load()
 end
 
 function love.update(dt)
-  if gameState == "menu" then
-      menu:update(dt)
-  elseif gameState == "settings" then
-      settings:update(dt)
-  elseif gameState == "playing" then
-      players:update(dt)
-      tower:update()
-      enemy:update(dt)
-      loadmap:update(dt)
-      --shoot:update(dt)
-  end
+    if gameState == "menu" then
+        menu:update(dt)
+    elseif gameState == "settings" then
+        settings:update(dt)
+    elseif gameState == "playing" then
+        players:update(dt)
+        tower:update()
+        enemy:update(dt)
+        loadmap:update(dt)
+        --shoot:update(dt)
+    end
 end
 
 function love.draw()
-  if gameState == "menu" then
-      menu:draw()
-  elseif gameState == "settings" then
-      settings:draw()
-  elseif gameState == "playing" then
-      loadmap:draw()
-      tower:draw()
-      enemy:draw()
-      --shoot:draw()
-      players:draw()
-  end
+    if gameState == "menu" then
+        menu:draw()
+    elseif gameState == "settings" then
+        settings:draw()
+    elseif gameState == "playing" then
+        loadmap:draw()
+        tower:draw()
+        enemy:draw()
+        --shoot:draw()
+        players:draw()
+    end
 end
 
 
 function love.keypressed(key, scancode, isrepeat)
-  if gameState == "menu" then
-      if key == activeKeybinds.up then
-        print("menu up")
-          menu.selected = menu.selected - 1
-          if menu.selected < 1 then menu.selected = #menu.options end
-      elseif key == activeKeybinds.down then
-          menu.selected = menu.selected + 1
-          if menu.selected > #menu.options then menu.selected = 1 end
-      elseif key == activeKeybinds.select then
-          if menu.selected == 1 then
-              gameState = "playing"
-              --activeKeybinds = {} -- No specific keybinds for playing -- TODO: 
-          elseif menu.selected == 2 then
-              gameState = "settings"
-              activeKeybinds = keybinds.settings
-          elseif menu.selected == 3 then
-              love.event.quit()
-          end
-      end
-  elseif gameState == "settings" then
-      if key == activeKeybinds.back then
-          gameState = "menu"
-          activeKeybinds = keybinds.menu
-      end
-  end
+    if gameState == "menu" then
+        if key == activeKeybinds.up then
+            print("menu up")
+            menu.selected = menu.selected - 1
+            if menu.selected < 1 then menu.selected = #menu.options end
+        elseif key == activeKeybinds.down then
+            menu.selected = menu.selected + 1
+            if menu.selected > #menu.options then menu.selected = 1 end
+        elseif key == activeKeybinds.select then
+            if menu.selected == 1 then
+                gameState = "playing"
+                --activeKeybinds = {} -- No specific keybinds for playing -- TODO: 
+            elseif menu.selected == 2 then
+                gameState = "settings"
+                activeKeybinds = keybinds.settings
+            elseif menu.selected == 3 then
+                love.event.quit()
+            end
+        end
+    elseif gameState == "settings" then
+        if key == activeKeybinds.back then
+            gameState = "menu"
+            activeKeybinds = keybinds.menu
+        end
+    end
 end
