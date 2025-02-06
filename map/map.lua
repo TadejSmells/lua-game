ba870f358518458764ad34b7bb11029367231062
Tadej <tadej.kadivec1@gmail.com>
2025-02-06 12:16:37 +0100

    map changes

--- map/map.lua
+++ map/map.lua
@@ -17,7 +17,7 @@ function map:createMap(fileName)
         self.tiles = {}
         self.tileRatio = ratio
 
-        for i = 0, 9 do
+        for i = 0, 10 do
             self.tiles[i] = love.graphics.newQuad(
                 i * self.baseTileSize, 0, self.baseTileSize, self.baseTileSize, self.tileset:getDimensions()
             )
